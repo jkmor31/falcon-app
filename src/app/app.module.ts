6898ff5acf1128308d9b216ae6b9d241cbba7c29
James Morical <77235874+jkmor31@users.noreply.github.com>
2022-01-19 13:49:14 -0600

    add home and ira components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { IraComponent } from './ira/ira.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IraComponent
   ],
   imports: [
     BrowserModule,

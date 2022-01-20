5d3057e4836893a979a5da17e7387adc1478e232
James Morical <77235874+jkmor31@users.noreply.github.com>
2022-01-20 09:54:43 -0600

    add dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { IraComponent } from './ira/ira.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: "home", component: HomeComponent},
-  {path: "ira", component: IraComponent}
+  {path: "ira", component: IraComponent},
+  {path: "dashboard", component: DashboardComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { IraComponent } from './ira/ira.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    IraComponent
+    IraComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
